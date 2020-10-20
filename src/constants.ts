a410805ebc9f6beeb5c9c1449466d1be0c70ad91
Thomas Kosciuch <tkosciuch@adaptavist.com>
2020-10-20 19:11:02 -0400

    step 1 of o-auth flow

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,13 @@
 /** params:
- *  clientId and secret come from https://developer.spotify.com/dashboard/ to get them, register an app! 
+ *  clientId and secret come from https://developer.spotify.com/dashboard/ to get them, register an app!
  *  url is a redirect required for o-auth
  */
+
 export const envVars = {
   clientId: process.env.client_id,
   clientSecret: process.env.clien_secret,
-  url: process.env.url,
+  redirectUri: process.env.redirectUri,
 };
+
+export const spotifyScope =
+  "user-read-playback-state user-read-currently-playing";
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import querystring from "querystring";
 
-import { envVars } from "./constants";
+import { envVars, spotifyScope } from "./constants";
+import { randomStr } from "./authFunctions/randomStr";
 
 let app = express();
 app.use(cookieParser());
 app.use(cors());
 
-app.get("song", () => {
-  console.log("hat");
+const stateKey = "spotify_bpm_state";
+
+app.get("/song", (req, res) => {
+  const stateStr = randomStr(16);
+  res.cookie(stateKey, stateStr);
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: envVars.clientId,
+        scope: spotifyScope,
+        redirect_uri: envVars.redirectUri,
+        state: stateStr,
+      })
+  );
 });
 
 const portNumber = 3000;
