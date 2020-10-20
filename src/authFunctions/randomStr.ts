a79d6732a06f662c1ce01307c5ca6124a90973d6
Thomas Kosciuch <tkosciuch@adaptavist.com>
2020-10-20 19:13:18 -0400

    forgot to add auth funcitons

---
+++ src/authFunctions/randomStr.ts
@@ -0,0 +1,10 @@
+export const randomStr = (length: number): string => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
